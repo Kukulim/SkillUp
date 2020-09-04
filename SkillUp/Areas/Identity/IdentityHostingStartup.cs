9518a8d30412a30ae85c089c23e6f3373e2b1a7e
kukulim <beckon@interia.pl>
2020-09-04 13:38:52 +0200

    override identy views add dashboard main page & controller

---
+++ SkillUp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SkillUp.Data;
+using SkillUp.Models.ApplicationUser;
+
+[assembly: HostingStartup(typeof(SkillUp.Areas.Identity.IdentityHostingStartup))]
+namespace SkillUp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- SkillUp/Controllers/HomeController.cs
+++ SkillUp/Controllers/HomeController.cs
@@ -20,6 +20,10 @@ namespace SkillUp.Controllers
 
         public IActionResult Index()
         {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                return Redirect("/Dashboard/Index");
+            }
             return View();
         }
 
