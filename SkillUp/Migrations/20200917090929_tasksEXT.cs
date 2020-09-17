c9743275641cbb904b5db7afa09bee2bc64fd766
kukulim <beckon@interia.pl>
2020-09-17 11:11:13 +0200

    task model + partialview

---
+++ SkillUp/Migrations/20200917090929_tasksEXT.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SkillUp.Migrations
+{
+    public partial class tasksEXT : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Completed",
+                table: "Tasks",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CompletedAt",
+                table: "Tasks",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Tasks",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Completed",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "CompletedAt",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Tasks");
+        }
+    }
+}
--- SkillUp/Models/TasksModel.cs
+++ SkillUp/Models/TasksModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,11 @@ namespace SkillUp.Models
     {
         public int Id { get; set; }
         public string Content { get; set; }
+        public bool Completed { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime CompletedAt { get; set; }
+
+        [ScaffoldColumn(false)]
         public string UserId { get; set; }
     }
 }
